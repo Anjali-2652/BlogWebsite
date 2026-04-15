dd083282eb1207ab30f685fc80edbe322d6b8e9a
Anjali Gupta <guptaanjali2652@gmail.com>
2026-04-15 22:16:31 +0545

    day 2 made register api

--- Backend/models/user.model.js
+++ Backend/models/user.model.js
@@ -1,52 +1,56 @@
 import mongoose, { mongo } from "mongoose";
 
-const userSchema = new mongoose.Schema({
-    firstName:{
-        type : String,
-        required : true
+const userSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
     },
-    lastName:{
-        type: String,
-        required : trusted,
+    lastName: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    bio: {
+      type: String,
+      default: "",
+    },
+    occupation: {
+      type: String,
+      default: "",
+    },
+    photo: {
+      type: String,
+      default: "",
+    },
+    instagram: {
+      type: String,
+      default: "",
+    },
+    facebook: {
+      type: String,
+      default: "",
+    },
+    linkedIn: {
+      type: String,
+      default: "",
+    },
+    github: {
+      type: String,
+      default: "",
+    },
+  },
+  { timestamps: true },
+);
 
-    },
-    email:{
-        type: String,
-        required :true,
-        unique : true
-    },
-    password:{
-        type: String,
-        required : true
-    },
-    bio:{
-        type: String,
-        default : ""
-    },
-    occupation:{
-        type: String,
-        default : ""
-    },
-    photo:{
-        type: String,
-        default : ""
-    },
-    instagram:{
-        type: String,
-        default : ""
-    },
-     facebook:{
-        type: String,
-        default : ""
-    },
-     linkedIn:{
-        type: String,
-        default : ""
-    },
-     github:{
-        type: String,
-        default : ""
-    },
-},{timestamps : true})
+const User = mongoose.model("User", userSchema);
 
-const User = mongoose.model("User", userSchema)
+export { User };
--- Backend/server.js
+++ Backend/server.js
@@ -1,12 +1,19 @@
 import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./database/db.js";
+import userRoute from "./routes/user.route.js";
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 
 dotenv.config();
 const PORT = process.env.PORT || 3000;
 
+app.use("/api/v1/user", userRoute);
+// "http://localhost:8000/api/v1/user/register"
+
 app.listen(PORT, () => {
-  connectDB()
+  connectDB();
   console.log(`Server is running on port ${PORT}`);
 });
