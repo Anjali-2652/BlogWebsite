572c72ebd4d37dcf9d04d9ec5224bf3254c0cde4
Anjali Gupta <guptaanjali2652@gmail.com>
2026-04-19 22:27:17 +0545

    day 6 page updated

--- Frontend/src/Pages/Home.jsx
+++ Frontend/src/Pages/Home.jsx
@@ -1,11 +1,12 @@
-import React from 'react'
-import { Button } from '../Components/ui/button'
+import React from "react";
+import { Button } from "../Components/ui/button";
+import Hero from "../Components/Hero";
 
 export const Home = () => {
   return (
-    <div>Home
-      <Button> click me !!!</Button>
+    <div className="pt-40">
+      <Hero />
     </div>
-  )
-}
-export default Home
+  );
+};
+export default Home;
