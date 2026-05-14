ae9d8a1365a01582d2d5e3501d71daaa2c37a74c
Anjali Gupta <guptaanjali2652@gmail.com>
2026-05-14 12:57:23 +0545

    ui error check

--- Frontend/src/Pages/Profile.jsx
+++ Frontend/src/Pages/Profile.jsx
@@ -2,6 +2,11 @@ import React from 'react'
 
 export const Profile = () => {
   return (
-    <div>Profile</div>
+    <div className=''>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Esse sint distinctio saepe dicta incidunt. Quo eligendi aut veniam impedit voluptas numquam illum eveniet. Obcaecati laborum voluptatibus ipsum, natus dolorum dolore.
+      Libero ipsa obcaecati assumenda quibusdam inventore, praesentium ullam, unde soluta rem sed nihil, cum minima enim commodi dignissimos repellendus eaque iure porro! Laboriosam vitae totam eaque quibusdam architecto dolore sed.
+      Aspernatur nulla eos minima nesciunt velit officia rerum quae. Aut ipsam, numquam earum officia facilis minima illum ullam cum doloremque explicabo, omnis vitae modi suscipit quam animi perspiciatis quis dicta?
+      Ipsum nulla dicta vitae et! Fugiat odit quaerat omnis. Veniam officiis eaque error sit rem odio eius qui deserunt laudantium, ullam corporis deleniti alias quibusdam quia. Maiores eligendi error cum.</p>
+    </div>
   )
 }
